92dfd8d54e4aee307bc55fc954ab4ed20e5171e2
Manish Adhikari <101424844+manishadhikaryy@users.noreply.github.com>
2024-06-29 20:46:19 -0400

    Designed the UI for the home page

--- Healthier/ContentView.swift
+++ Healthier/ContentView.swift
@@ -1,61 +1,121 @@
-//
-//  ContentView.swift
-//  Healthier
-//
-//  Created by Manish Adhikari on 6/29/24.
-//
-
 import SwiftUI
-import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+        ZStack {
+            Color.white
+            
+            VStack {
+                Text("Healthier")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                    .shadow(color: .gray, radius: 2, x: 0, y: 2)
+                    .padding(.bottom, 40)
+                VStack {
+                    Image("kcal")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                    Text("Calories")
+                        .foregroundColor(.black)
+                        .font(.title2)
+                        .padding(.top, 8)
+                    Text("300")
+                }
+                .background (
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(.gray.opacity(0.1))
+                        .frame(width: 100, height: 140)
+                    )
+                
+                HStack(spacing: 60) {
+                    VStack {
+                        Image("carb")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50)
+                        Text("Carbs")
+                            .foregroundColor(.black)
+                            .font(.headline)
+                            .padding(.top, 8)
+                        Text("50g")
+                    }
+                    .background (
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(.gray.opacity(0.1))
+                            .frame(width: 100, height: 140)
+
+                    )
+                    VStack {
+                        Image("protein")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50)
+                        Text("Protein")
+                            .foregroundColor(.black)
+                            .font(.headline)
+                            .padding(.top, 8)
+                        Text("50g")
+
+                    }
+                    .background (
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(.gray.opacity(0.1))
+                            .frame(width: 100, height: 140)
+
+                    )
+                    VStack {
+                        Image("fat")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50)
+                        Text("Fat")
+                            .foregroundColor(.black)
+                            .font(.headline)
+                            .padding(.top, 8)
+                        Text("50g")
+
+                    }
+                    .background (
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(.gray.opacity(0.1))
+                            .frame(width: 100, height: 140)
+                    )
+                }
+                .padding(.top, 50)
+                
+                VStack(spacing: 20) {
+                    VStack {
+                        Text("Macronutrients")
+                            .font(.headline)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                            .foregroundColor(.white)
+                            .shadow(radius: 5)
+                    }
+                    
+                    VStack {
+                        Text("Workout")
+                            .font(.headline)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                            .foregroundColor(.white)
+                            .shadow(radius: 5)
                     }
                 }
-                .onDelete(perform: deleteItems)
+                .padding(.vertical, 50)
+                .padding(.bottom, 50)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
+            .padding(.horizontal, 30)
         }
     }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
